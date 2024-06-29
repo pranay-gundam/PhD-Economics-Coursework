77d7532b724c184478b900a0cf96d44165faee96
Yanarp Madnug <pranaygundam00@gmail.com>
2024-06-29 08:40:25 -0700

    miscelaneous changes

--- Job Searching/julia/agents.jl
+++ Job Searching/julia/agents.jl
@@ -1,12 +1,18 @@
 struct Agent
     name::String
-    temporal_discount::Float64
+    temporal_discount::AbstractFloat
     utility_func::Function
+    unemploy_benefit::AbstractFloat 
 end
 
 
-function make_Agent(name::String, temporal_discount::Float64, utility_func::Function)
-    return Agent(name, temporal_discount, utility_func)
+function Agent(name::String; temporal_discount::Float64 = 0.9, 
+                             utility_func::Function = identity, 
+                             unemploy_benefit::Float64 = 0.0)
+    if temporal_discount < 0 || temporal_discount > 1
+        throw(ArgumentError("Temporal discount must be between 0 and 1"))
+    end
+    return Agent(name, temporal_discount, utility_func, unemploy_benefit)
 end
 
 function get_name(agent::Agent)
@@ -21,6 +27,9 @@ function get_utility_func(agent::Agent)
     return agent.utility_func
 end
 
+function get_unemploy_benefit(agent::Agent)
+    return agent.unemploy_benefit
+end
 
 
 
--- Job Searching/julia/test.jl
+++ Job Searching/julia/test.jl
@@ -1,27 +1,28 @@
 # Loading in package files
 include("agents.jl")
+include("job_search.jl")
 
 # Creating basic primitives to use
 
 
 # Testing Agent creation and modification
-A1 = make_Agent("Bob", 0.5, log)
-A2 = make_Agent("Bobby", 0.999, sqrt)
+A1 = Agent("Bob"; temporal_discount = 0.5, utility_func = log)
+A2 = Agent("Bobby"; temporal_discount = 0.999, utility_func = sqrt)
 
-@assert get_name(A1) == "Bob"
-@assert get_name(A2) == "Bobby"
+@test get_name(A1) == "Bob"
+@test get_name(A2) == "Bobby"
 
-@assert get_temporal_discount(A1) == 0.5
-@assert get_temporal_discount(A2) == 0.999
+@test get_temporal_discount(A1) == 0.5
+@test get_temporal_discount(A2) == 0.999
 
-@assert get_utility_func(A1) == log
-@assert get_utility_func(A2) == sqrt
+@test get_utility_func(A1) == log
+@test get_utility_func(A2) == sqrt
 
-@assert get_utility_func(A1)(exp(3)) == 3
-@assert get_utility_func(A1)(1) == 0
+@test get_utility_func(A1)(exp(3)) == 3
+@test get_utility_func(A1)(1) == 0
 
-@assert get_utility_func(A2)(4) == 2.0
-@assert get_utility_func(A2)(0) == 0
+@test get_utility_func(A2)(4) == 2.0
+@test get_utility_func(A2)(0) == 0
 
 # Testing Job Search object creation and modification
 
